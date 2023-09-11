75ce062b3a887ad272c8f00d6bce6083097830dd
Alex Verbovskiy <cofeek5@gmail.com>
2023-09-11 20:29:59 +0300

    some fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 fs.readFile("./index.html", function (err, html) {
   if (err) {
